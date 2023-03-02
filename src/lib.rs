0a7b5d60bc6ff2fbfa63bcb83e0d5befe6fee86b
jgetzel <jgetzel@ucsd.edu>
2023-03-02 13:14:00 -0800

    Added headless command line option for server

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
-use bevy::app::{App, Plugin};
+use std::default::Default;
+use std::env;
+use bevy::app::{App, Plugin, PluginGroupBuilder, ScheduleRunnerPlugin};
+use bevy::audio::AudioPlugin;
+use bevy::core_pipeline::CorePipelinePlugin;
+use bevy::diagnostic::DiagnosticsPlugin;
+use bevy::input::InputPlugin;
 use bevy::log::LogPlugin;
 use bevy::prelude::*;
-use bevy_editor_pls::EditorPlugin;
+use bevy::render::RenderPlugin;
+use bevy::scene::ScenePlugin;
+use bevy::sprite::SpritePlugin;
+use bevy::text::TextPlugin;
+use bevy::time::TimePlugin;
+use bevy::ui::UiPlugin;
+use bevy::winit::WinitPlugin;
 use bevy_egui::EguiPlugin;
 use bevy_embedded_assets::EmbeddedAssetPlugin;
 use scenes::AppState;
@@ -14,7 +26,7 @@ use crate::networking::server::ServerPlugin;
 use crate::object::SyncedObjects;
 use crate::physics::PhysicsPlugin;
 use crate::player::{PlayerInput, PlayerPlugin};
-use crate::scenes::ScenePlugin;
+use crate::scenes::TankScenePlugin;
 use crate::sprite_updater::SpriteUpdatePlugin;
 
 mod asset_loader;
@@ -33,12 +45,7 @@ pub struct ClientExecutablePlugin;
 
 impl Plugin for ClientExecutablePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(DefaultPlugins
-            .set(get_window_plugin("Client Window"))
-            .set(get_log_plugin())
-            .build()
-            .add_before::<AssetPlugin, _>(EmbeddedAssetPlugin)
-        )
+        app
             .add_plugin(DefaultExecutablePlugin)
             .add_plugin(ClientPlugin);
     }
@@ -48,12 +55,7 @@ pub struct ServerExecutablePlugin;
 
 impl Plugin for ServerExecutablePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(DefaultPlugins
-            .set(get_window_plugin("Server Window"))
-            .set(get_log_plugin())
-            .build()
-            .add_before::<AssetPlugin, _>(EmbeddedAssetPlugin)
-        )
+        app
             .add_plugin(DefaultExecutablePlugin)
             .add_plugin(ServerPlugin);
 
@@ -64,13 +66,32 @@ struct DefaultExecutablePlugin;
 
 impl Plugin for DefaultExecutablePlugin {
     fn build(&self, app: &mut App) {
+        app
+            .add_plugin(CorePlugin::default())
+            .add_plugin(TimePlugin::default());
+
+        app.add_plugin(get_log_plugin())
+            .add_plugin(TransformPlugin::default())
+            .add_plugin(HierarchyPlugin::default())
+            .add_plugin(DiagnosticsPlugin::default())
+            .add_plugin(EmbeddedAssetPlugin::default())
+            .add_plugin(AssetPlugin::default())
+            .add_plugin(ScenePlugin);
+
+        if env::args().all(|arg| arg != "headless") {
+            app.add_plugins(NonHeadlessPlugins)
+                .add_plugin(EguiPlugin);
+        }
+        else {
+            app.add_plugin(ScheduleRunnerPlugin::default());
+        }
+
         app.insert_resource(Lobby::default())
             .insert_resource(SyncedObjects::default());
 
         app.add_plugin(AssetLoaderPlugin)
             .add_plugin(NetworkPlugin)
-            .add_plugin(EguiPlugin)
-            .add_plugin(ScenePlugin)
+            .add_plugin(TankScenePlugin)
             .add_plugin(GameCameraPlugin)
             .add_plugin(PlayerPlugin)
             .add_plugin(BulletPlugin)
@@ -86,15 +107,15 @@ impl Plugin for DefaultExecutablePlugin {
 }
 
 fn get_log_plugin() -> LogPlugin {
-    // // this code is compiled only if debug assertions are enabled (debug mode)
-    // #[cfg(debug_assertions)]
-    // return LogPlugin {
-    //     level: bevy::log::Level::DEBUG,
-    //     filter: "debug,wgpu_core=warn,wgpu_hal=warn,mygame=debug".into(),
-    // };
-    //
-    // // this code is compiled only if debug assertions are disabled (release mode)
-    // #[cfg(not(debug_assertions))]
+    // this code is compiled only if debug assertions are enabled (debug mode)
+    #[cfg(debug_assertions)]
+    return LogPlugin {
+        level: bevy::log::Level::DEBUG,
+        filter: "debug,wgpu_core=warn,wgpu_hal=warn,mygame=debug".into(),
+    };
+
+    // this code is compiled only if debug assertions are disabled (release mode)
+    #[cfg(not(debug_assertions))]
     return LogPlugin {
         level: bevy::log::Level::INFO,
         filter: "info,wgpu_core=warn,wgpu_hal=warn".into(),
@@ -108,5 +129,24 @@ fn get_window_plugin(title: &str) -> WindowPlugin {
             ..default()
         },
         ..default()
+    }
+}
+
+struct NonHeadlessPlugins;
+
+impl PluginGroup for NonHeadlessPlugins {
+    fn build(self) -> PluginGroupBuilder {
+        PluginGroupBuilder::start::<Self>()
+            .add(InputPlugin::default())
+            .add(WindowPlugin::default())
+            .add(WinitPlugin::default())
+            .add(RenderPlugin::default())
+            .add(ImagePlugin::default())
+            .add(CorePipelinePlugin::default())
+            .add(SpritePlugin::default())
+            .add(TextPlugin::default())
+            .add(AudioPlugin::default())
+            .add(GilrsPlugin::default())
+            .add(AnimationPlugin::default())
     }
 }

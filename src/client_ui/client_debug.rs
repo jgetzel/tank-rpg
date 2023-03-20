ed1cdab1940c929b33942dee242664b8bb4b029e
jgetzel <jgetzel@ucsd.edu>
2023-03-20 12:47:58 -0700

    Added client debug toggle

--- src/client_ui/client_debug.rs
+++ src/client_ui/client_debug.rs
@@ -1,5 +1,6 @@
 use bevy::app::{App, Plugin};
 use bevy::prelude::*;
+use bevy::utils::petgraph::visit::Walker;
 use bevy_egui::{egui, EguiContexts};
 use crate::AppState;
 use crate::simulation::SyncedObjects;
@@ -9,15 +10,35 @@ pub struct ClientDebugUIPlugin;
 
 impl Plugin for ClientDebugUIPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(show_player_lobby.run_if(in_state(AppState::InGame)));
+        app
+            .insert_resource(DebugToggle::default())
+            .add_system(debug_toggle_sys)
+            .add_system(show_player_lobby.run_if(in_state(AppState::InGame)));
     }
+}
+
+#[derive(Resource, Default)]
+pub struct DebugToggle {
+    debug_mode: bool
+}
+
+fn debug_toggle_sys(
+    inputs: Res<Input<ScanCode>>,
+    mut debug_toggle: ResMut<DebugToggle>
+) {
+    if inputs.just_pressed(ScanCode(0x29)) {
+        debug_toggle.debug_mode = !debug_toggle.debug_mode;
+    };
 }
 
 fn show_player_lobby(
     mut egui_ctx: EguiContexts,
+    debug_mode: Res<DebugToggle>,
     lobby: Res<Lobby>,
     objects: Res<SyncedObjects>
 ) {
+    if !debug_mode.debug_mode { return; }
+
     egui::Window::new("Client Info")
         .show(egui_ctx.ctx_mut(), |ui| {
             ui.label("Lobby");

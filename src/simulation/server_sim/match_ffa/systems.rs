0894ca35e2df7c5dec9fad888638a5726418be03
jgetzel <jgetzel@ucsd.edu>
2023-03-20 16:40:48 -0700

    Update K/D on scene init

--- src/simulation/server_sim/match_ffa/systems.rs
+++ src/simulation/server_sim/match_ffa/systems.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::{Commands, EventReader, EventWriter, NextState, Res, ResMut, Time};
 use crate::AppState;
+use crate::simulation::Lobby;
 use crate::simulation::server_sim::InGameState;
 use crate::simulation::server_sim::match_ffa::{MATCH_LENGTH_SECS, MatchTimer, OnMatchTimerFinishedEvent, OnRestartMatchTimerFinishedEvent, RESTART_WAIT_SECS, RestartMatchTimer};
 
@@ -54,5 +55,14 @@ pub fn new_match_on_restart_timer(
     events.iter().for_each(|_| {
         next_in_game_state.set(InGameState::Playing);
         next_app_state.set(AppState::InGame);
+    });
+}
+
+pub fn clear_player_scores_on_exit(
+    mut lobby: ResMut<Lobby>,
+) {
+    lobby.player_data.values_mut().for_each(|data| {
+        data.deaths = 0;
+        data.kills = 0;
     });
 }

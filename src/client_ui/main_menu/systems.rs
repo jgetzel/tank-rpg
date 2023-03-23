9d85c8629363ab7a5719569d98d8403cc5cd95b9
jgetzel <jgetzel@ucsd.edu>
2023-03-22 23:11:17 -0700

    Main Menu rework

--- src/client_ui/main_menu/systems.rs
+++ src/client_ui/main_menu/systems.rs
@@ -1,19 +1,21 @@
+use crate::client_ui::main_menu::ui::{MainMenuExt, CENTER_WIDTH, MAIN_MENU_FRAME};
+use crate::client_ui::main_menu::{
+    CenterMenuState, ConnectState, OnConnectAttempt, OnHostAttempt, ServerIPInput, ServerPortInput,
+};
+use crate::server_networking::{DEFAULT_SERVER_HOSTNAME, DEFAULT_SERVER_PORT};
+use crate::utils::prefabs::default_camera;
+use crate::AppState;
+use bevy::log::info;
 use bevy::prelude::{Commands, EventReader, EventWriter, NextState, ResMut};
+use bevy_egui::egui::{Align2, Button, RichText};
+use bevy_egui::{egui, EguiContexts};
+use bevy_quinnet::client::certificate::CertificateVerificationMode;
 use bevy_quinnet::client::connection::{ConnectionConfiguration, ConnectionEvent};
 use bevy_quinnet::client::Client;
-use bevy::log::info;
-use std::net::{Ipv4Addr, SocketAddr};
-use bevy_quinnet::client::certificate::CertificateVerificationMode;
-use bevy_egui::{egui, EguiContexts};
-use bevy_egui::egui::{Align2, RichText};
-use std::str::FromStr;
-use bevy_quinnet::server::{Server, ServerConfiguration};
 use bevy_quinnet::server::certificate::CertificateRetrievalMode;
-use crate::AppState;
-use crate::client_ui::main_menu::{ConnectState, OnConnectAttempt, OnHostAttempt, ServerIPInput, ServerPortInput};
-use crate::client_ui::main_menu::ui::{CENTER_WIDTH, MAIN_MENU_FRAME, MainMenuExt};
-use crate::server_networking::{DEFAULT_SERVER_HOSTNAME};
-use crate::utils::prefabs::default_camera;
+use bevy_quinnet::server::{Server, ServerConfiguration};
+use std::net::{Ipv4Addr, SocketAddr};
+use std::str::FromStr;
 
 pub fn init(mut commands: Commands) {
     commands.spawn(default_camera());
@@ -23,6 +25,7 @@ pub fn main_menu_gui(
     mut contexts: EguiContexts,
     mut server_ip_string: ResMut<ServerIPInput>,
     mut server_port_string: ResMut<ServerPortInput>,
+    mut center_menu_state: ResMut<CenterMenuState>,
     connect_writer: EventWriter<OnConnectAttempt>,
     host_writer: EventWriter<OnHostAttempt>,
 ) {
@@ -31,23 +34,30 @@ pub fn main_menu_gui(
         .show(contexts.ctx_mut(), |ui| {
             MAIN_MENU_FRAME.show(ui, |ui| {
                 ui.set_width(CENTER_WIDTH);
-
-                ui.connect_menu(&mut server_ip_string.0, connect_writer);
-
-                ui.vertical_centered(|ui| {
-                    ui.add_space(10.);
-                    ui.label(RichText::new("- OR -").strong());
-                    ui.add_space(10.);
-                });
-
-                ui.host_menu(&mut server_port_string.0, host_writer);
+                match *center_menu_state {
+                    CenterMenuState::Main => {
+                        ui.center_menu(&mut center_menu_state);
+                    }
+                    CenterMenuState::Join => {
+                        ui.connect_menu(
+                            &mut center_menu_state,
+                            &mut server_ip_string.0,
+                            connect_writer,
+                        );
+                    }
+                    CenterMenuState::Host => {
+                        ui.host_menu(
+                            &mut center_menu_state,
+                            &mut server_port_string.0,
+                            host_writer
+                        );
+                    }
+                }
             });
         });
 }
 
-pub fn connecting_gui(
-    mut contexts: EguiContexts,
-) {
+pub fn connecting_gui(mut contexts: EguiContexts) {
     egui::Area::new("Main Menu Connecting Area")
         .anchor(Align2::CENTER_CENTER, [0., 0.])
         .show(contexts.ctx_mut(), |ui| {
@@ -61,15 +71,20 @@ pub fn connect_attempt_listener(
     mut client: ResMut<Client>,
 ) {
     events.iter().for_each(|e| {
-        info!("Attempting to connect to Socket Address {}...", e.address.to_string());
+        info!(
+            "Attempting to connect to Socket Address {}...",
+            e.address.to_string()
+        );
         next_state.set(ConnectState::Connecting);
-        client.open_connection(
-            ConnectionConfiguration::from_addrs(
-                e.address,
-                SocketAddr::from_str("0.0.0.0:0").unwrap(),
-            ),
-            CertificateVerificationMode::SkipVerification,
-        ).unwrap();
+        client
+            .open_connection(
+                ConnectionConfiguration::from_addrs(
+                    e.address,
+                    SocketAddr::from_str("0.0.0.0:0").unwrap(),
+                ),
+                CertificateVerificationMode::SkipVerification,
+            )
+            .unwrap();
     });
 }
 
@@ -83,24 +98,24 @@ pub fn host_attempt_listener(
         info!("Attempting to host on port {}...", e.port_num);
         next_state.set(ConnectState::StartingServer);
 
-        server.start_endpoint(
-            ServerConfiguration::from_ip(
-                Ipv4Addr::new(0, 0, 0, 0).into(), e.port_num),
-            CertificateRetrievalMode::GenerateSelfSigned {
-                server_hostname: DEFAULT_SERVER_HOSTNAME.to_string()
-            },
-        ).unwrap();
+        server
+            .start_endpoint(
+                ServerConfiguration::from_ip(Ipv4Addr::new(0, 0, 0, 0).into(), e.port_num),
+                CertificateRetrievalMode::GenerateSelfSigned {
+                    server_hostname: DEFAULT_SERVER_HOSTNAME.to_string(),
+                },
+            )
+            .unwrap();
 
-        client.open_connection(
-            ConnectionConfiguration::from_addrs(
-                SocketAddr::new(
-                    Ipv4Addr::from_str("127.0.0.1").unwrap().into(),
-                    e.port_num,
+        client
+            .open_connection(
+                ConnectionConfiguration::from_addrs(
+                    SocketAddr::new(Ipv4Addr::from_str("127.0.0.1").unwrap().into(), e.port_num),
+                    SocketAddr::from_str("0.0.0.0:0").unwrap(),
                 ),
-                SocketAddr::from_str("0.0.0.0:0").unwrap(),
-            ),
-            CertificateVerificationMode::SkipVerification,
-        ).unwrap();
+                CertificateVerificationMode::SkipVerification,
+            )
+            .unwrap();
     })
 }
 
--- src/client_ui/main_menu/ui.rs
+++ src/client_ui/main_menu/ui.rs
@@ -1,77 +1,133 @@
-use std::net::SocketAddr;
-use std::str::FromStr;
+use crate::client_ui::main_menu::{CenterMenuState, OnConnectAttempt, OnHostAttempt};
+use crate::server_networking::DEFAULT_SERVER_PORT;
 use bevy::prelude::EventWriter;
 use bevy::utils::default;
 use bevy_egui::egui;
-use bevy_egui::egui::*;
 use bevy_egui::egui::epaint::Shadow;
+use bevy_egui::egui::*;
 use once_cell::sync::Lazy;
-use crate::client_ui::main_menu::{OnConnectAttempt, OnHostAttempt};
-use crate::server_networking::DEFAULT_SERVER_PORT;
+use std::net::SocketAddr;
+use std::str::FromStr;
+use bevy::log::info;
+use bevy::window::CursorIcon::Default;
 
 pub const CENTER_WIDTH: f32 = 300.;
 
-pub static MAIN_MENU_FRAME: Lazy<Frame> = Lazy::new(||
-    Frame {
-        inner_margin: Margin::from(10.),
-        rounding: Rounding::from(5.0),
-        shadow: Shadow {
-            extrusion: 4.,
-            color: Color32::from_black_alpha(50),
-        },
-        fill: Visuals::dark().window_fill(),
-        ..default()
-    }
-);
+pub static MAIN_MENU_FRAME: Lazy<Frame> = Lazy::new(|| Frame {
+    inner_margin: Margin::from(10.),
+    rounding: Rounding::from(5.0),
+    shadow: Shadow {
+        extrusion: 4.,
+        color: Color32::from_black_alpha(50),
+    },
+    fill: Visuals::dark().window_fill(),
+    ..default()
+});
 
 pub trait MainMenuExt {
-    fn connect_menu(&mut self, server_ip: &mut String, connect_writer: EventWriter<OnConnectAttempt>);
+    fn center_menu(&mut self, center_menu_state: &mut CenterMenuState);
 
-    fn host_menu(&mut self, server_port: &mut String, host_writer: EventWriter<OnHostAttempt>);
+    fn connect_menu(
+        &mut self,
+        center_menu_state: &mut CenterMenuState,
+        server_ip: &mut String,
+        connect_writer: EventWriter<OnConnectAttempt>,
+    );
+
+    fn host_menu(
+        &mut self,
+        center_menu_state: &mut CenterMenuState,
+        server_port: &mut String,
+        host_writer: EventWriter<OnHostAttempt>,
+    );
 }
 
 impl MainMenuExt for Ui {
-    fn connect_menu(&mut self, server_ip: &mut String, mut connect_writer: EventWriter<OnConnectAttempt>) {
+    fn center_menu(&mut self, center_menu_state: &mut CenterMenuState) {
         self.vertical_centered(|ui| {
-            ui.menu_button(RichText::new("Join Server").heading(), |ui| {
-                ui.label("Enter Server IP:");
-                ui.horizontal_top(|ui| {
-                    ui.text_edit_singleline(server_ip);
-                    let address = SocketAddr::from_str(server_ip.as_str());
-                    if ui.add_enabled(
-                        SocketAddr::from_str(server_ip.as_str()).is_ok(),
-                        Button::new("Connect!"),
-                    ).clicked() {
-                        connect_writer.send(OnConnectAttempt { address: address.unwrap() });
-                    }
-                });
-                if ui.button("Back").clicked() {
-                    ui.close_menu();
-                }
-            });
+            if ui.button(RichText::new("Join Server").heading()).clicked() {
+                *center_menu_state = CenterMenuState::Join;
+            }
+
+            ui.add_space(10.);
+            ui.label(RichText::new("- OR -").strong());
+            ui.add_space(10.);
+
+            if ui.button(RichText::new("Host Server").heading()).clicked() {
+                *center_menu_state = CenterMenuState::Host;
+            }
         });
     }
 
-    fn host_menu(&mut self, server_port: &mut String, mut host_writer: EventWriter<OnHostAttempt>) {
+    fn connect_menu(
+        &mut self,
+        center_menu_state: &mut CenterMenuState,
+        server_ip: &mut String,
+        mut connect_writer: EventWriter<OnConnectAttempt>,
+    ) {
         self.vertical_centered(|ui| {
-            ui.menu_button(RichText::new("Host Server").heading(), |ui| {
-                ui.label("Enter Port Number:");
-                ui.horizontal_top(|ui| {
-                    ui.add(egui::TextEdit::singleline(server_port)
-                        .hint_text(DEFAULT_SERVER_PORT.to_string()));
+            ui.label("Enter Server IP:");
+            ui.horizontal_top(|ui| {
 
-                    let port_num = server_port.parse::<u16>();
-                    if ui.add_enabled(
-                        port_num.is_ok(),
-                        Button::new("Host!")
-                    ).clicked() {
-                        host_writer.send(OnHostAttempt { port_num: port_num.unwrap()});
-                    }
-                });
-                if ui.button("Back").clicked() {
-                    ui.close_menu();
+                let placeholder = format!("127.0.0.1:{}", DEFAULT_SERVER_PORT);
+                ui.add(
+                    egui::TextEdit::singleline(server_ip)
+                        .hint_text(placeholder.clone())
+                );
+
+                let address = if server_ip.is_empty() {
+                    SocketAddr::from_str(placeholder.as_str())
+                } else { SocketAddr::from_str(server_ip.as_str()) };
+
+                if ui
+                    .add_enabled(
+                        address.is_ok(),
+                        Button::new("Connect!"),
+                    )
+                    .clicked()
+                {
+                    connect_writer.send(OnConnectAttempt {
+                        address: address.unwrap(),
+                    });
                 }
             });
+            if ui.button("Back").clicked() {
+                *center_menu_state = CenterMenuState::Main;
+            }
+        });
+    }
+
+    fn host_menu(
+        &mut self,
+        center_menu_state: &mut CenterMenuState,
+        server_port: &mut String,
+        mut host_writer: EventWriter<OnHostAttempt>
+    ) {
+        self.vertical_centered(|ui| {
+            ui.label("Enter Port Number:");
+            ui.horizontal_top(|ui| {
+                ui.add(
+                    egui::TextEdit::singleline(server_port)
+                        .hint_text(DEFAULT_SERVER_PORT.to_string()),
+                );
+
+                let port_num = if server_port.is_empty() {
+                    Ok(DEFAULT_SERVER_PORT)
+                }
+                else { server_port.parse::<u16>() };
+
+                if ui
+                    .add_enabled(port_num.is_ok(), Button::new("Host!"))
+                    .clicked()
+                {
+                    host_writer.send(OnHostAttempt {
+                        port_num: port_num.unwrap(),
+                    });
+                }
+            });
+            if ui.button("Back").clicked() {
+                *center_menu_state = CenterMenuState::Main;
+            }
         });
     }
 }
